896df8e6dd26b085e7dbb12c75cb5321166207ff
Gürkan Dizen <gurkandizen17@gmail.com>
2026-07-27 17:39:30 +0300

    gun-08: Statik olan Menü arayüzü sekmeli (tabs) etkleşimli arayüze dönüştürüldü

--- hafta-2/gun-08-dom-1/js/tabs.js
+++ hafta-2/gun-08-dom-1/js/tabs.js
@@ -1,4 +1,17 @@
 export function initTabs() {
-    console.log("initTabs çalıştı");
+    const menuTabs = document.querySelectorAll(".menu-tab");
+    const menuPanels = document.querySelectorAll(".menu-panel");
+
+    menuTabs.forEach(tab => tab.addEventListener("click", () => {
+        menuTabs.forEach(t => t.classList.remove("is-active"));
+        menuPanels.forEach(panel => panel.classList.remove("is-active"));
+
+        tab.classList.add("is-active");
+
+        menuPanels.forEach(panel => {
+            if(panel.dataset.panel === tab.dataset.tab) {
+                panel.classList.add("is-active");
+            }
+        });
+    }));
 }
-console.log("tabs js yüklendi");
